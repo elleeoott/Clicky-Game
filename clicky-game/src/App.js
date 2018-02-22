99ba48feba0ef5f4fefb92c1c65d0dc82bc830d3
Evan Darst <evan.l.darst@gmail.com>
2018-02-22 11:36:56 -0500

    created wrapper directory

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cats from './cats.json'
+import Wrapper from './components/Wrapper'
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
